f272cbe5fe957a4b1064d053d8d3b4f8789b20ba
vincentzyc <vincentzyc@qq.com>
2021-02-28 10:29:25 +0800

    ✨feat: axios

---
+++ src/plugins/axios.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+// Full config:  https://github.com/axios/axios#request-config
+// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+const config = {
+  // baseURL: process.env.baseURL || process.env.apiUrl || ""
+  // timeout: 60 * 1000, // Timeout
+  // withCredentials: true, // Check cross-site Access-Control
+};
+
+const _axios = axios.create(config);
+
+_axios.interceptors.request.use(
+  (cfg) => {
+    // Do something before request is sent
+    return cfg;
+  },
+  (err) => {
+    // Do something with request error
+    return Promise.reject(err);
+  },
+);
+
+// Add a response interceptor
+_axios.interceptors.response.use(
+  (res) => {
+    // Do something with response data
+    return res;
+  },
+  (err) => {
+    // Do something with response error
+    return Promise.reject(err);
+  },
+);
+
+export default _axios;
