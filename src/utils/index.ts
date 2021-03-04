485c27046f0f01f4d918f262f2cacfd9a6ccb1fe
vincentzyc <vincentzyc@qq.com>
2021-03-04 10:01:29 +0800

    🎉init: init

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -60,56 +60,6 @@ export function getUrlParam(name: string = ''): UrlParamBack {
 }
 
 /**
-   * 滑动到指定位置的缓冲动画函数
-   * @param {Object} dom 目标dom
-   * @param {Number} destination 目标位置
-   * @param {Number} rate 缓动率(越大速度越慢)
-   * 示例用法
-     let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-     let dom = scrollTop === document.documentElement.scrollTop ? document.documentElement : document.body;
-     easeout(dom, 0);
-   */
-export function easeout(dom: HTMLElement, destination: number = 0, rate: number = 5): void {
-  let position = dom.scrollTop;
-  if (position === destination || typeof destination !== "number" || rate === 0) {
-    return;
-  }
-  // 不存在原生`requestAnimationFrame`，用`setTimeout`模拟替代
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function (fn) {
-      return setTimeout(fn, 17);
-    };
-  }
-  let step = function () {
-    position = position + (destination - position) / rate;
-    if (Math.abs(destination - position) < 1) {
-      //动画结束
-      dom.scrollTop = destination;
-      return;
-    }
-    dom.scrollTop = position;
-    requestAnimationFrame(step);
-  };
-  step();
-}
-/**
- * 滚动到指定元素
- * @param {Object} el 当前dom元素 
- * @param {Number} offset 元素距离顶部的偏移量
- */
-export function scrollIntoView(el: HTMLElement, offset: number = 200): void {
-  if (!el) return;
-  if (typeof offset !== 'number') return;
-  let clientRect = el.getBoundingClientRect();
-  let isElementInViewport = clientRect.top >= 0 && clientRect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
-  if (!isElementInViewport) {
-    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    let dom = scrollTop === document.documentElement.scrollTop ? document.documentElement : document.body;
-    let destination = (scrollTop + clientRect.top - offset) > 0 ? (scrollTop + clientRect.top - offset) : 0
-    easeout(dom, destination, 10)
-  }
-}
-/**
  * 执行js代码
  * @param {String} jscode js代码
  * @param {String} scriptid id标识（相同id只执行一次，不传id每次都会执行）
