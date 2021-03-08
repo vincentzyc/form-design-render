b5edc153cdb9995ee93ea8613ddaa8bb39c1e47e
vincentzyc <vincentzyc@qq.com>
2021-03-08 10:18:40 +0800

    🎉init: init

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,84 +1,79 @@
-// import Vue from "vue";
+import { ApiModule } from "./types"
 import apiModule1 from './apiModule1';
 import apiModule2 from './apiModule2';
+import Axios from '@/plugins/axios'
 
-// const vm = new Vue()
-
-const createInterface = arr => {
+const createInterface = (arr: ApiModule[]) => {
   let Interface = {};
   arr.forEach(v => {
-    Interface[v.name] = param => {
+    Interface[v.name] = (param: unknown) => {
       return new Promise(reslove => {
-        Api.axiosPost(v.url, param).then(res => reslove(res))
+        axiosPost(v.url, param).then(res => reslove(res))
       })
     }
-  });
+  })
   return Interface
 }
 
-const Api = {
-  ApiModule1: createInterface(apiModule1),
-  ApiModule2: createInterface(apiModule2),
-  env() {
-    if (process.env.NODE_ENV === "development") return "development";
-    return "production";
-  },
-  previewUrl() {
-    if (this.env() === "development") {
-      let arr = window.location.origin.split(":");
-      return `${arr[0]}:${arr[1]}:9000`
-    }
-    return window.location.origin + "/form-design-h5"
-  },
-  previewOrigin() {
-    if (this.env() === "development") {
-      let arr = window.location.origin.split(":");
-      return `${arr[0]}:${arr[1]}:9000`
-    }
-    return window.location.origin
-  },
-  apiUrl() {
-    return "http://xxx.com/";
-  },
-  axiosPost(url, data) {
-    return new Promise(resolve => {
-      vm.$axios.post(this.apiUrl() + url, data).then(res => {
-        resolve(res);
-      }).catch(error => {
-        console.log(error);
-      });
-    })
-  },
-  postMsgoUrl() {
-    if (this.env() === "development") {
-      let arr = window.location.origin.split(":");
-      return `${arr[0]}:${arr[1]}:9000`
-    }
-    return window.location.origin + "/form-design"
-  },
-  postMsgoOrigin() {
-    if (this.env() === "development") {
-      let arr = window.location.origin.split(":");
-      return `${arr[0]}:${arr[1]}:9000`
-    }
-    return window.location.origin
-  },
-  testPost() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve({ code: 0, msg: "成功", data: "success" });
-      }, 200);
-    });
-  },
-	/**
-	 * 获取验证码
-	 * @param phone  手机号
-	 */
-  getVerifyCode(phone) {
-    return new Promise(resolve => {
-      this.testPost(phone).then(res => resolve(res));
-    })
-  }
+export const ApiModule1 = createInterface(apiModule1)
+export const ApiModule2 = createInterface(apiModule2)
+export function env() {
+  if (process.env.NODE_ENV === "development") return "development";
+  return "production";
 }
-
-export default Api
+export function previewUrl() {
+  if (env() === "development") {
+    let arr = window.location.origin.split(":");
+    return `${arr[0]}:${arr[1]}:9000`
+  }
+  return window.location.origin + "/form-design-h5"
+}
+export function previewOrigin() {
+  if (env() === "development") {
+    let arr = window.location.origin.split(":");
+    return `${arr[0]}:${arr[1]}:9000`
+  }
+  return window.location.origin
+}
+export function apiUrl() {
+  return "http://xxx.com/";
+}
+export function axiosPost(url: string, data: unknown) {
+  return new Promise(resolve => {
+    Axios.post(apiUrl() + url, data).then(res => {
+      resolve(res);
+    }).catch(error => {
+      console.log(error);
+    });
+  })
+}
+export function postMsgoUrl() {
+  if (env() === "development") {
+    let arr = window.location.origin.split(":");
+    return `${arr[0]}:${arr[1]}:9000`
+  }
+  return window.location.origin + "/form-design"
+}
+export function postMsgoOrigin() {
+  if (env() === "development") {
+    let arr = window.location.origin.split(":");
+    return `${arr[0]}:${arr[1]}:9000`
+  }
+  return window.location.origin
+}
+export function testPost(data: unknown) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ code: 0, msg: "成功", data: "success", postData: data });
+    }, 200);
+  })
+}
+/**
+ * 获取验证码
+ * @param phone  手机号
+ */
+export function getVerifyCode(phone: string) {
+  return new Promise(resolve => {
+    testPost(phone).then(res => resolve(res));
+  })
+}
