e46c56b2653d63517d7c96a0ca7728b441ef25b5
vincentzyc <vincentzyc@qq.com>
2021-03-16 09:17:33 +0800

    🎉init: init

--- src/api/apiModule1.ts
+++ src/api/apiModule1.ts
@@ -1,17 +1,24 @@
 import { ApiModule } from "./types"
 
-const apiModule1: ApiModule[] = [{
-  name: "loginCheck", //方法名
-  url: "LoginCheck.aspx" //接口路径
+export interface ApiModuleList1 {
+  getVerifyCode(param: unknown): Promise<void>,
+  savePgae(param: unknown): Promise<void>,
+  getModelDetail(param: unknown): Promise<void>,
+  getRecordDetail(param: unknown): Promise<void>,
+}
+
+export const apiModule1: ApiModule[] = [{
+  name: "getVerifyCode", //方法名
+  url: "/service/model/getVerifyCode" //接口路径
 }, {
   name: "savePgae",
-  url: "service/model/savePgae"
+  url: "/service/model/savePgae"
 }, {
   name: "getModelDetail",
-  url: "service/model/model_details"
+  url: "/service/model/model_details"
 }, {
   name: "getRecordDetail",
-  url: "service/model/package_details"
+  url: "/service/model/package_details"
 }]
 
 export default apiModule1
