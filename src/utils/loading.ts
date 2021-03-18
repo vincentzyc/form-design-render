b5135fee4d5bcb5955f9ed5791e219d55a7cb5df
vincentzyc <vincentzyc@qq.com>
2021-03-18 08:58:57 +0800

    🎉init: init

--- src/utils/loading.ts
+++ src/utils/loading.ts
@@ -3,8 +3,9 @@ import { Toast } from 'vant';
 export function openLoading(text: string): void {
   Toast.loading({
     message: text,
-    duration: 0,
-    forbidClick: true,
+    duration: 0,       // 持续展示 toast
+    forbidClick: true, // 禁用背景点击
+    overlay: true,     //显示背景遮罩层
   });
 }
 
