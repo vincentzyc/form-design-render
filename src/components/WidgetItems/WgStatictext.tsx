0050ef64a1e935cdfb9bf3116645da4f3b2c6e6b
vincentzyc <vincentzyc@qq.com>
2021-04-04 09:29:21 +0800

    🎉init: init

---
+++ src/components/WidgetItems/WgStatictext.tsx
@@ -0,0 +1,33 @@
+import { changeRem, formatStyle } from '@/utils/format/unit';
+import { jumpLink } from '@/utils/validate/link';
+import { defineComponent } from '@vue/runtime-core';
+import { toRaw } from 'vue';
+// import CustomPopup from './wg-popup'
+
+export default defineComponent({
+  props: {
+    item: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  emits: ['update:item'],
+  setup(props, { emit }) {
+    const domStyle = {
+      backgroundColor: props.item.backgroundColor || '',
+      backgroundImage: props.item.backgroundImage ? `url(${props.item.backgroundImage})` : ''
+    }
+    const handleClick = () => {
+      if (props.item.link) return jumpLink(props.item.link)
+      if (props.item.popupList?.length > 0) {
+        emit('update:item', { ...props.item, showPopup: true })
+      }
+    }
+    return () => (
+      <div class="wg-staticText clearfix" style={domStyle} >
+        <p style={formatStyle(props.item.style)} innerHTML={changeRem(props.item.value)} onClick={handleClick}></p>
+        {/* <CustomPopup list={props.item.popupList || []} vModel={props.item.showPopup} /> */}
+      </div>
+    )
+  }
+})
