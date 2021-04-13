3653898682e9599d90f1c05cd94823cb08279082
vincentzyc <vincentzyc@qq.com>
2021-04-13 16:34:48 +0800

    🎉init: init

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,15 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     pageData: "",
+    wgList: [],
     valiPopupDate: false
   },
   mutations: {
     setPageData(state, payload) {
       state.pageData = payload;
+    },
+    setWgList(state, payload) {
+      state.wgList = payload
     },
     setValiPopupDate(state, payload) {
       state.valiPopupDate = payload;
