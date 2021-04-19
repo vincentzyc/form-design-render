2437e098d924e810be4b3648b2ae04b4e730ff50
vincentzyc <vincentzyc@qq.com>
2021-04-19 09:10:38 +0800

    🎉init: init

---
+++ src/components/WidgetItems/WgDatePicker.tsx
@@ -0,0 +1,77 @@
+import { defineComponent, ref } from 'vue';
+import { DatetimePicker } from 'vant';
+import { changeRem, formatStyle } from "@/utils/format/unit";
+import { useWgList } from '@/composition/use-wglist';
+
+export default defineComponent({
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const { wgData } = useWgList(props.item)
+
+    const minDate = new Date(1920, 0, 1)
+    const maxDate = new Date(2080, 12, 31)
+
+    const showPicker = ref(false)
+    const currentDate = ref(new Date())
+
+    const formatDate = (Date: Date) => {
+      const year = Date.getFullYear(),
+        month = Date.getMonth() + 1,
+        date = Date.getDate();
+      return `${year}-${month < 10 ? '0' + month : month}-${date < 10 ? '0' + date : date}`
+    }
+
+    const open = () => showPicker.value = true;
+
+    const close = () => showPicker.value = false;
+
+    const cancel = () => {
+      close()
+    }
+
+    const confirm = (value: Date) => {
+      wgData.value = formatDate(value);
+      close()
+    }
+
+    const wrapClass = ['wg-item wg-padding', wgData.label.labelPosition === 'top' ? 'flex-column' : 'align-middle']
+    return () => (
+      <div>
+        <div
+          class={wrapClass}
+          style={formatStyle(wgData.style)}
+        >
+          <div
+            class="wg-title"
+            v-show={wgData.showLabel}
+            style={{ width: changeRem(wgData.label.labelWidth) }}
+          >{wgData.label.labelTitle}</div>
+          <div class="flex-auto disabled-input" onClick={open}>
+            <input
+              disabled
+              id={wgData.key}
+              v-model={wgData.value}
+              placeholder={wgData.placeholder}
+              class="wg-input" />
+          </div>
+        </div>
+        <van-popup position="bottom" v-model={[showPicker.value, 'show']} teleport="body">
+          <DatetimePicker
+            v-model={currentDate.value}
+            type="date"
+            title="选择年月日"
+            min-date={minDate}
+            max-date={maxDate}
+            onCancel={cancel}
+            onConfirm={confirm}
+          />
+        </van-popup>
+      </div>
+    )
+  }
+})
