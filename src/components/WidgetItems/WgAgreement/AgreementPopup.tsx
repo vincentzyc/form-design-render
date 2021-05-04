fe657e4452eee0078cd8ac9178520b3069e6b894
vincentzyc <vincentzyc@qq.com>
2021-05-05 01:44:00 +0800

    🎉init: init

--- src/components/WidgetItems/WgAgreement/AgreementPopup.tsx
+++ src/components/WidgetItems/WgAgreement/AgreementPopup.tsx
@@ -1,6 +1,4 @@
 // 弹框组件（支持iframe嵌入页面）
-// 使用例子： <AgreementPopup ref="agreement" />
-// 打开弹框：this.$refs.agreement.open(text);
 import { textBr } from '@/utils/format/string';
 import { isLink } from '@/utils/validate/link';
 import { defineComponent, ref } from 'vue';
