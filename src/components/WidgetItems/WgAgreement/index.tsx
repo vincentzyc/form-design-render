526f82d5d18f0a01879521b4aa66c8738ea72fab
vincentzyc <vincentzyc@qq.com>
2021-05-05 01:36:36 +0800

    🎉init: init

---
+++ src/components/WidgetItems/WgAgreement/index.tsx
@@ -0,0 +1,45 @@
+import { defineComponent, ref } from 'vue'
+import { formatStyle } from '@/utils/format/unit';
+import { Checkbox } from 'vant';
+import AgreementPopup from './AgreementPopup'
+export default defineComponent({
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const elAgreement = ref()
+    const openAgreement = (text, event) => {
+      event.stopPropagation();
+      elAgreement.value.open(text);
+    }
+    const getAgreementList = () => {
+      return (
+        <Checkbox v-model={props.item.value} style={formatStyle(props.item.style)} shape="square">
+          <p style={{ color: props.item.style.color }}>
+            <span>{props.item.tipText}</span>
+            {props.item.titleTexts.map((titleText, key) => (
+              <span key={key}>
+                {key > 0 && <span>和</span>}
+                {titleText.title && (
+                  <span
+                    style={{ color: props.item.agreementColor }}
+                    onClick={event => openAgreement(titleText.text, event)}
+                  >{titleText.title}</span>
+                )}
+              </span>
+            ))}
+          </p>
+        </Checkbox>
+      )
+    }
+    return () => (
+      <div class="wg-agreement clearfix" id={props.item.key}>
+        {getAgreementList()}
+        <AgreementPopup ref={elAgreement.value} />
+      </div>
+    )
+  }
+})
