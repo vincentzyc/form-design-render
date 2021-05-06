3ecb8e8aee63d1ecbfb403c1a7cd42b4d2bf4c9a
vincentzyc <vincentzyc@qq.com>
2021-05-07 00:45:17 +0800

    🎉init: init

--- src/components/WidgetItems/WgRandomCode/random-code.ts
+++ src/components/WidgetItems/WgRandomCode/random-code.ts
@@ -3,7 +3,7 @@ const familyStyleList = ["Helvetica Neue", "Helvetica", "PingFang SC", "Hiragino
 
 //所有候选组成验证码的字符，当然也可以用中文的
 const codeChars = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
@@ -13,7 +13,7 @@ export function createCode() {
   const codeLength = 4; //验证码的长度
   let code = "";
 
-  for (var i = 0; i < codeLength; i++) {
+  for (let i = 0; i < codeLength; i++) {
     const charNum = Math.floor(Math.random() * 62);
     code += codeChars[charNum];
   }
