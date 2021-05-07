328af784ac00b116af3f0418f2bafcc0b7969600
vincentzyc <vincentzyc@qq.com>
2021-05-08 00:33:22 +0800

    🎉init: init

--- src/components/WidgetItems/WgCheckbox.tsx
+++ src/components/WidgetItems/WgCheckbox.tsx
@@ -1,5 +1,6 @@
 import { defineComponent } from 'vue'
 import { changeRem, formatStyle } from "@/utils/format/unit";
+import { useWgList } from '@/composition/use-wglist';
 
 export default defineComponent({
   props: {
@@ -9,18 +10,20 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const wrapClass = ['wg-item', 'flex-wrap', 'wg-checkbox', props.item.label.labelPosition === 'top' ? 'flex-column' : 'align-middle'];
+    const { wgData } = useWgList(props.item)
+
+    const wrapClass = ['wg-item', 'flex-wrap', 'wg-checkbox', wgData.label.labelPosition === 'top' ? 'flex-column' : 'align-middle'];
     return () => (
-      <div class={wrapClass} style={formatStyle(props.item.style)}>
-        <div class="wg-title" style={{ width: changeRem(props.item.label.labelWidth) }}>{props.item.label.labelTitle}</div>
-        <div class="flex-auto" id={props.item.key}>
-          {props.item.options.map((optionsItem: string, key: number) => (
+      <div class={wrapClass} style={formatStyle(wgData.style)}>
+        <div class="wg-title" style={{ width: changeRem(wgData.label.labelWidth) }}>{wgData.label.labelTitle}</div>
+        <div class="flex-auto" id={wgData.key}>
+          {wgData.options.map((optionsItem: string, key: number) => (
             <label class="label" key={optionsItem + key}>
               <input
                 class="wg-checkbox-input"
-                type={props.item.isRadio ? 'radio' : 'checkbox'}
+                type={wgData.isRadio ? 'radio' : 'checkbox'}
                 value={optionsItem}
-                v-model={props.item.value}
+                v-model={wgData.value}
                 style="display:none"
               />
               <span>{optionsItem}</span>
