9bb196726f8410c61ee0ef1a629550a307b00c47
vincentzyc <vincentzyc@qq.com>
2021-05-13 00:10:06 +0800

    🎉init: init

--- src/components/WidgetItems/WgRandomCode/index.tsx
+++ src/components/WidgetItems/WgRandomCode/index.tsx
@@ -1,6 +1,7 @@
 import { defineComponent } from 'vue'
 import createCode from './random-code'
 import { changeRem, formatStyle } from "@/utils/format/unit";
+import { useWgList } from '@/composition/use-wglist';
 
 export default defineComponent({
   props: {
@@ -10,24 +11,26 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const directionClass = props.item.label.labelPosition === 'top' ? 'flex-column' : 'align-middle';
+    const { wgData } = useWgList(props.item)
+
+    const directionClass = wgData.label.labelPosition === 'top' ? 'flex-column' : 'align-middle';
     const getCode = () => {
-      let randomData = createCode()
-      props.item.codeValue = randomData.code;
-      props.item.style.btnStyle.fontFamily = randomData.font;
+      const randomData = createCode()
+      wgData.codeValue = randomData.code;
+      wgData.style.btnStyle.fontFamily = randomData.font;
     }
     getCode()
     return () => (
-      <div class="wg-random-code" style={formatStyle(props.item.style)}>
+      <div class="wg-random-code" style={formatStyle(wgData.style)}>
         <div class={['wg-item', directionClass]}>
           <div
             class="wg-title flex-none"
-            v-show={props.item.showLabel}
-            style={{ width: changeRem(props.item.label.labelWidth) }}
-          >{props.item.label.labelTitle}</div>
+            v-show={wgData.showLabel}
+            style={{ width: changeRem(wgData.label.labelWidth) }}
+          >{wgData.label.labelTitle}</div>
           <div class="flex flex-auto align-middle">
-            <input placeholder={props.item.placeholder} type="text" maxlength={4} v-model={[props.item.value, ['trim']]} class="wg-input flex-auto" />
-            <button class="random-code flex-none" style={formatStyle(props.item.style.btnStyle)} onClick={getCode}>{props.item.codeValue}</button>
+            <input placeholder={wgData.placeholder} type="text" maxlength={4} v-model={[wgData.value, ['trim']]} class="wg-input flex-auto" />
+            <button class="random-code flex-none" style={formatStyle(wgData.style.btnStyle)} onClick={getCode}>{wgData.codeValue}</button>
           </div>
         </div>
       </div>
