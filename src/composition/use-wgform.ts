9502f0e4d2a63a04fef6efee4bb4dd6d9a8ae859
vincentzyc <vincentzyc@qq.com>
2021-05-16 12:21:07 +0800

    🎉init: init

--- src/composition/use-wgform.ts
+++ src/composition/use-wgform.ts
@@ -1,28 +1,35 @@
-import { computed } from "vue"
+import { computed, reactive } from "vue"
 import { useStore } from 'vuex';
 
 export function useWgForm(item: Record<string, any>): Record<string, any> {
   const store = useStore()
-  const wgList = computed(() => store.state.wgList)
-  const index = wgList.value.findIndex((v: { key: string }) => v.key === item.key)
-  if (index > -1) {
-    wgList.value.splice(index, 1, item)
-    return {
-      wgData: wgList.value[index],
-      wgIndex: index
-    }
-  } else {
-    wgList.value.push(item)
-    const wgIndex = wgList.value.length - 1
-    return {
-      wgData: wgList.value[wgIndex],
-      wgIndex: wgIndex
-    }
+  const formData = computed(() => store.state.formData)
+  if (item.apiKey) {
+    formData.value[item.apiKey] = item.value || ''
   }
+  return {
+    formData: formData.value,
+    wgData: reactive(item)
+  }
+  // const store = useStore()
+  // const wgList = computed(() => store.state.wgList)
+  // const index = wgList.value.findIndex((v: { key: string }) => v.key === item.key)
+  // if (index > -1) {
+  //   wgList.value.splice(index, 1, item)
+  //   return {
+  //     wgData: wgList.value[index],
+  //     wgIndex: index
+  //   }
+  // } else {
+  //   wgList.value.push(item)
+  //   const wgIndex = wgList.value.length - 1
+  //   return {
+  //     wgData: wgList.value[wgIndex],
+  //     wgIndex: wgIndex
+  //   }
+  // }
 }
-
-export default useWgForm
 
 // use 
 // import { useWgForm } from '@/composition/use-wgform'
-// const { wgData } = useWgForm(props.item)
+// const { wgData,formData } = useWgForm(props.item)
