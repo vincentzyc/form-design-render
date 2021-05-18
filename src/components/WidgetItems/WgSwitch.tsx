c4c0156adb47a13524c1ecd24915d1e494b02511
vincentzyc <vincentzyc@qq.com>
2021-05-18 10:47:23 +0800

    🎉init: init

--- src/components/WidgetItems/WgSwitch.tsx
+++ src/components/WidgetItems/WgSwitch.tsx
@@ -10,13 +10,13 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const { wgData } = useWgForm(props.item)
+    const { wgData, formData } = useWgForm(props.item)
     const wrapClass = ['wg-item', 'wg-switch', wgData.label.labelPosition === 'top' ? 'flex-column' : 'align-middle']
     return () => (
       <div class={wrapClass} style={wgData.style}>
         <div class="wg-title" style={{ width: changeRem(wgData.label.labelWidth) }}>{wgData.label.labelTitle}</div>
         <label class="label" id={wgData.key}>
-          <input type="checkbox" v-model={wgData.value} class="wg-switch-input" style="display:none" />
+          <input type="checkbox" v-model={formData[wgData.apiKey]} class="wg-switch-input" style="display:none" />
           <span class="wg-switch-core"></span>
         </label>
       </div>
