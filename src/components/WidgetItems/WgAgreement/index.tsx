8797d19048646321829b050821b231340284662f
vincentzyc <vincentzyc@qq.com>
2021-05-28 00:10:44 +0800

    🎉init: init

--- src/components/WidgetItems/WgAgreement/index.tsx
+++ src/components/WidgetItems/WgAgreement/index.tsx
@@ -3,6 +3,7 @@ import { formatStyle } from '@/utils/format/unit'
 import { Checkbox } from 'vant'
 import AgreementPopup from './AgreementPopup'
 import { TitleText } from './types'
+import { useWgForm } from '@/composition/use-wgform'
 
 export default defineComponent({
   props: {
@@ -13,21 +14,23 @@ export default defineComponent({
   },
   setup(props) {
     const elAgreement = ref()
+    const { wgData, formData } = useWgForm(props.item)
+
     const openAgreement = (text: string, event: MouseEvent) => {
       event.stopPropagation();
       elAgreement.value.open(text);
     }
     const getAgreementList = () => {
       return (
-        <Checkbox v-model={props.item.value} style={formatStyle(props.item.style)} shape="square">
-          <p style={{ margin: 0, color: props.item.style.color }}>
-            <span>{props.item.tipText}</span>
-            {props.item.titleTexts.map((titleText: TitleText, key: number) => (
+        <Checkbox v-model={formData[wgData.apiKey]} style={formatStyle(wgData.style)} shape="square">
+          <p style={{ margin: 0, color: wgData.style.color }}>
+            <span>{wgData.tipText}</span>
+            {wgData.titleTexts.map((titleText: TitleText, key: number) => (
               <span key={key}>
                 {key > 0 && <span>和</span>}
                 {titleText.title && (
                   <span
-                    style={{ color: props.item.agreementColor }}
+                    style={{ color: wgData.agreementColor }}
                     onClick={event => openAgreement(titleText.text, event)}
                   >{titleText.title}</span>
                 )}
@@ -38,7 +41,7 @@ export default defineComponent({
       )
     }
     return () => (
-      <div class="wg-agreement clearfix wg-padding" id={props.item.key}>
+      <div class="wg-agreement clearfix wg-padding" id={wgData.key}>
         {getAgreementList()}
         <AgreementPopup ref={elAgreement} />
       </div>
