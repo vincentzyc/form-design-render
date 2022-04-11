4eed49d818162fbc9788ef60b5532f3722275fba
vincentzyc <vincentzyc@qq.com>
2022-04-11 15:15:27 +0800

    ✨ feat: add wgTabs

--- src/composition/use-wgform.ts
+++ src/composition/use-wgform.ts
@@ -14,25 +14,48 @@ export function useWgForm(item: Record<string, any>) {
     wgForms: wgForms.value,
     wgData: item
   }
-  // const store = useStore()
-  // const wgForms = computed(() => store.state.wgForms)
-  // const index = wgForms.value.findIndex((v: { key: string }) => v.key === item.key)
-  // if (index > -1) {
-  //   wgForms.value.splice(index, 1, item)
-  //   return {
-  //     wgData: wgForms.value[index],
-  //     wgIndex: index
-  //   }
-  // } else {
-  //   wgForms.value.push(item)
-  //   const wgIndex = wgForms.value.length - 1
-  //   return {
-  //     wgData: wgForms.value[wgIndex],
-  //     wgIndex: wgIndex
-  //   }
-  // }
 }
 
-// use 
+// export function useRemoveWgForm(list: Record<string, any>[]) {
+//   const store = useStore()
+//   const formData = computed(() => store.state.formData)
+//   const wgForms = computed(() => store.state.wgForms)
+//   for (let index = 0; index < list.length; index++) {
+//     const item = list[index];
+//     for (let formIndex = 0; formIndex < wgForms.value.length; formIndex++) {
+//       const formItem = wgForms.value[formIndex];
+//       if (formItem.key === item.key) {
+//         wgForms.value.splice(formIndex, 1)
+//         delete formData.value[formItem.apiKey]
+//         delete formData.value[formItem.codeKey]
+//       }
+//     }
+//   }
+//   console.log(formData, wgForms);
+//   return {
+//     formData: formData.value,
+//     wgForms: wgForms.value
+//   }
+// }
+
+// const store = useStore()
+// const wgForms = computed(() => store.state.wgForms)
+// const index = wgForms.value.findIndex((v: { key: string }) => v.key === item.key)
+// if (index > -1) {
+//   wgForms.value.splice(index, 1, item)
+//   return {
+//     wgData: wgForms.value[index],
+//     wgIndex: index
+//   }
+// } else {
+//   wgForms.value.push(item)
+//   const wgIndex = wgForms.value.length - 1
+//   return {
+//     wgData: wgForms.value[wgIndex],
+//     wgIndex: wgIndex
+//   }
+// }
+
+// use
 // import { useWgForm } from '@/composition/use-wgform'
 // const { wgData,formData } = useWgForm(props.item)
