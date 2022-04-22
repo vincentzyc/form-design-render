e1701cb0e507c671e8a9cae4a08b31c3093f0b77
vincentzyc <vincentzyc@qq.com>
2022-04-22 21:06:21 +0800

    ✨ feat: add wgTabs

---
+++ src/components/WidgetItems/WgTypes/Tabs.ts
@@ -0,0 +1,17 @@
+  export interface TypesTabs {
+  type: "Tabs",
+  name: "Tabs标签",
+  value: 0,
+  activeTab: 0,
+  wgClassName: "wg-formlist widget-active",
+  list: [{
+    title: "标签一",
+    list: []
+  }, {
+    title: "标签二",
+    list: []
+  }],
+  style: {
+    margin: "0px 0px 0px 0px"
+  }
+}
