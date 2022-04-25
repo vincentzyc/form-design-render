6be5d25c042b651a679c2af3118d830fc3ad652d
vincentzyc <vincentzyc@qq.com>
2022-04-26 00:48:10 +0800

    ✨ feat: widget types

--- src/components/WidgetItems/WgTypes/StaticText.ts
+++ src/components/WidgetItems/WgTypes/StaticText.ts
@@ -1,13 +1,13 @@
 export interface TypesStaticText {
-  type: string,
-  name: string,
-  value: string,
-  link: string,
-  popupList: Record<any, any>[],
-  showPopup: false,
-  backgroundImage: string,
-  backgroundColor: string,
+  type: string, // StaticText
+  name: string, // 文本描述
+  value: string, // 这是一段文本
+  link: string, // ""
+  popupList: Record<any, any>[], // []
+  showPopup: false, // false
+  backgroundImage: string, // ""
+  backgroundColor: string, // ""
   style: {
-    margin: string
+    margin: string // 0px 0px 0px 0px
   }
 }
