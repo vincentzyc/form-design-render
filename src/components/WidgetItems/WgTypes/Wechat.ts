88e581d18c50f02dc4dfd2f119cc1a98baa56f85
vincentzyc <vincentzyc@qq.com>
2022-05-01 01:51:05 +0800

    ✨ feat: widget types

--- src/components/WidgetItems/WgTypes/Wechat.ts
+++ src/components/WidgetItems/WgTypes/Wechat.ts
@@ -10,20 +10,20 @@ export interface TypesWechat {
   backgroundColor: string, // "",
   positionFixed: 'auto' | 'top' | 'bottom',
   fixedTypes: ['auto', 'top', 'bottom'], // auto--正常 top--顶部悬浮  custom--自定义位置悬浮  bottom--底部悬浮
-  scrollHeight: 0,
+  scrollHeight: number, // 0,
   style: {
-    isImgBtn: false,
-    value: "",
+    isImgBtn: boolean, // false,
+    value: string, // "",
     margin: string, // "0px 0px 0px 0px",
     color: string, // "#333",
-    fontsize: 14,
+    fontsize: number, // 14,
     fontSize: string, // "14px",
     btnStyle: {
       borderRadius: string, // '20px',
-      borderradius: 20,
+      borderradius: number, // 20,
       backgroundColor: string, // "#01cd0d",
       color: string, // "#fff",
-      fontsize: 14,
+      fontsize: number, // 14,
       fontSize: string, // "14px"
     }
   }
