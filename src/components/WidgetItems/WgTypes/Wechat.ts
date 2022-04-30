23005a86856c902a2af9317938543a8d7f792c15
vincentzyc <vincentzyc@qq.com>
2022-05-01 01:06:27 +0800

    ✨ feat: widget types

--- src/components/WidgetItems/WgTypes/Wechat.ts
+++ src/components/WidgetItems/WgTypes/Wechat.ts
@@ -1,30 +1,30 @@
 export interface TypesWechat {
-  type: "Wechat",
-  name: "微信关注",
-  value: "微信账号",
-  wechatName: "",
-  btnText: "+  关注",
-  logo: "",
-  showName: true,
-  showLogo: true,
-  backgroundColor: "",
-  positionFixed: 'auto', 
+  type: string, // "Wechat",
+  name: string, // "微信关注",
+  value: string, // "微信账号",
+  wechatName: string, // "",
+  btnText: string, // "+  关注",
+  logo: string, // "",
+  showName: boolean, // true,
+  showLogo: boolean, // true,
+  backgroundColor: string, // "",
+  positionFixed: 'auto' | 'top' | 'bottom',
   fixedTypes: ['auto', 'top', 'bottom'], // auto--正常 top--顶部悬浮  custom--自定义位置悬浮  bottom--底部悬浮
   scrollHeight: 0,
   style: {
     isImgBtn: false,
     value: "",
-    margin: "0px 0px 0px 0px",
-    color: "#333",
+    margin: string, // "0px 0px 0px 0px",
+    color: string, // "#333",
     fontsize: 14,
-    fontSize: "14px",
+    fontSize: string, // "14px",
     btnStyle: {
-      borderRadius: '20px',
+      borderRadius: string, // '20px',
       borderradius: 20,
-      backgroundColor: "#01cd0d",
-      color: "#fff",
+      backgroundColor: string, // "#01cd0d",
+      color: string, // "#fff",
       fontsize: 14,
-      fontSize: "14px"
+      fontSize: string, // "14px"
     }
   }
 }
