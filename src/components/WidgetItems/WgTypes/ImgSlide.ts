d8293af09e77bf2451030bda344b947ea643155d
vincentzyc <vincentzyc@qq.com>
2022-05-02 00:31:00 +0800

    ✨ feat: widget types

---
+++ src/components/WidgetItems/WgTypes/ImgSlide.ts
@@ -0,0 +1,13 @@
+export interface TypesImgSlide {
+  type: string, // "ImgSlide",
+  name: string, // "图片轮播",
+  value: [{
+    url: string, // "https://www.baidu.com",
+    image: string, // "static/img/theme1.jpg"
+  }],
+  interval: number, // 3000,
+  style: {
+    margin: string, // "0px 0px 0px 0px",
+    height: number, // 250
+  }
+}
