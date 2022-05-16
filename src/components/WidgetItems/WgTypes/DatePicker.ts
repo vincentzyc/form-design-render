68fd25e7510780ea9c9c8252326c434facde3629
vincentzyc <vincentzyc@qq.com>
2022-05-16 09:15:37 +0800

    ✨ feat: widget types

---
+++ src/components/WidgetItems/WgTypes/DatePicker.ts
@@ -0,0 +1,19 @@
+export interface TypesDatePicker {
+  key: string,
+  type: "DatePicker",
+  name: "日期选择器",
+  label: {
+    labelTitle: "生日",
+    labelwidth: 50,
+    labelWidth: "50px",
+    labelPosition: "left" | "top"
+  },
+  apiKey: "birthday",
+  value: "",
+  showLabel: true,
+  placeholder: "请选择日期",
+  fieldTypes: "dateTypes",
+  style: {
+    margin: "0px 0px 0px 0px"
+  }
+}
