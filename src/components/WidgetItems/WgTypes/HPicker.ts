199ad69437f6e9f573570917bb7fdc88ce932528
vincentzyc <vincentzyc@qq.com>
2022-05-31 11:07:44 +0800

    ✨ feat: widget types

--- src/components/WidgetItems/WgTypes/HPicker.ts
+++ src/components/WidgetItems/WgTypes/HPicker.ts
@@ -1,25 +1,25 @@
 export interface TypesHPicker {
   key: string,
-  type: "HPicker",
-  name: "横向滑动单选",
-  value: "20",
-  options: ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"],
+  type: string, // "HPicker",
+  name: string, // "横向滑动单选",
+  value: string, // "20",
+  options: string[], // ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"],
   label: {
-    labelTitle: "年龄",
+    labelTitle: string, // "年龄",
     labelPosition: "left" | "top",
-    labelwidth: 50,
-    labelWidth: "50px"
+    labelwidth: number, // 50,
+    labelWidth: string, // "50px"
   },
-  fieldTypes: "hPickerTypes",
-  apiKey: "age",
-  itemWidth: 50,
-  showNumber: 5,
+  fieldTypes: string, // "hPickerTypes",
+  apiKey: string, // "age",
+  itemWidth: number, // 50,
+  showNumber: number, // 5,
   pickerStyle: {
-    color: "#409eff",
-    fontsize: 18,
-    fontSize: "18px"
+    color: string, // "#409eff",
+    fontsize: number, // 18,
+    fontSize: string, // "18px"
   },
   style: {
-    margin: "0px 0px 0px 0px"
+    margin: string, // "0px 0px 0px 0px"
   }
 }
