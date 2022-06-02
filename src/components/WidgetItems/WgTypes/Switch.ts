52b79c66deb1bdb19f7c910e5bb087898b9cce91
vincentzyc <vincentzyc@qq.com>
2022-06-02 09:05:59 +0800

    ✨ feat: widget types

--- src/components/WidgetItems/WgTypes/Switch.ts
+++ src/components/WidgetItems/WgTypes/Switch.ts
@@ -1,17 +1,17 @@
 export interface TypesSwitch {
   key: string,
-  type: "Switch",
-  name: "开关",
-  value: false,
+  type: string, // "Switch",
+  name: string, // "开关",
+  value: boolean, // false,
   label: {
-    labelTitle: "是否有信用卡",
+    labelTitle: string, // "是否有信用卡",
     labelPosition: "left" | "top",
-    labelwidth: 100,
-    labelWidth: "100px"
+    labelwidth: number, // 100,
+    labelWidth: string, // "100px"
   },
-  fieldTypes: "switchTypes",
-  apiKey: "creditCard",
+  fieldTypes: string, // "switchTypes",
+  apiKey: string, // "creditCard",
   style: {
-    margin: "0px 0px 0px 0px"
+    margin: string, // "0px 0px 0px 0px"
   }
 }
