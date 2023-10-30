5da894cebb1a8e2e089d9c2222ddb2824e9b6a46
vincentzyc <vincentzyc@qq.com>
2023-10-30 17:56:38 +0800

    🐳chore: 更新依赖

--- src/utils/loading.ts
+++ src/utils/loading.ts
@@ -1,14 +1,14 @@
-import { Toast } from 'vant';
+import { showLoadingToast, closeToast } from 'vant';
 
 export function openLoading(text: string): void {
-  Toast.loading({
+  showLoadingToast({
     message: text,
-    duration: 0,       // 持续展示 toast
+    duration: 0, // 持续展示 Toast
     forbidClick: true, // 禁用背景点击
-    overlay: true,     //显示背景遮罩层
+    overlay: true, //显示背景遮罩层
   });
 }
 
 export function closeLoading(): void {
-  Toast.clear();
+  closeToast();
 }
